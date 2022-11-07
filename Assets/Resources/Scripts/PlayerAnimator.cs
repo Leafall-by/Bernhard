f8d4c09279f2d60e00f6878fd07a4cee2db89144
betrayal <vogistv@gmail.com>
2022-11-07 17:34:27 +0700

    replace model player

--- Assets/Resources/Scripts/PlayerAnimator.cs
+++ Assets/Resources/Scripts/PlayerAnimator.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour
 {
     private Animator _animator;
-
-    public void Init()
+    private float _speedAnimation;
+    private float velocity = 0;
+    public void Init(float speedAnimation)
     {
         _animator = GetComponent<Animator>();
+        _speedAnimation = speedAnimation;
     }
 
     public void StartRun(float speedX, float speedZ)
@@ -16,10 +18,18 @@ public class PlayerAnimator : MonoBehaviour
         if (speedX != 0 || speedZ != 0)
         {
             _animator.SetBool("RunActive", true);
+
+            velocity += Time.deltaTime * _speedAnimation / 10;
+            velocity = velocity > 1 ? 1 : velocity;
+            _animator.SetFloat("Blend", velocity);
         }
         else
         {
             _animator.SetBool("RunActive", false);
+            
+            velocity -= Time.deltaTime * _speedAnimation / 10;
+            velocity = velocity < 0 ? 0 : velocity;
+            _animator.SetFloat("Blend", velocity);
         }
     }
 }
